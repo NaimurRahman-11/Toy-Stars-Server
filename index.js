2a303a798cca0e509671d0138e0bd837fcd49ac2
Naimur Rahman <nirzon.naim@gmail.com>
2023-05-18 12:16:39 -0400

    Added post and get server side data

--- index.js
+++ index.js
@@ -25,13 +25,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      
+      const toysCollection = client.db('toyDB').collection('toys');
+
+      app.post('/toys', async (req, res) => {
+          const newToy = req.body;
+          console.log(newToy);
+          const result = await toysCollection.insertOne(newToy);
+          res.send(result);
+      })
+
+
+      app.get('/toys', async (req, res) => {
+          const cursor = toysCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+
+      
+      
+      
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
